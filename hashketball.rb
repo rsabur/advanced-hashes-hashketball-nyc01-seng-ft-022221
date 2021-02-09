6ea7f7735239728f2e94d8eea0f8360d52eab634
Radiyah Abdus-Sabur <rsabur58@gmail.com>
2021-02-09 18:17:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -176,8 +176,8 @@ def player_numbers(team)
   while count < game_hash[:home][:players].length
     if team == game_hash[:home][:team_name]
       jersey_num = game_hash[:home][:players][count][:number]
-      brooklyn_numbers.push(jersey_num)
     end
+    brooklyn_numbers.push(jersey_num)
     count += 1
   end
 
