f99f121a5057b46aeb1dc92b1eb3cb62b55b3eb7
Radiyah Abdus-Sabur <rsabur58@gmail.com>
2021-02-09 16:39:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -176,14 +176,14 @@ def player_numbers(team)
   charlotte_numbers = []
   while count < game_hash[:home][:players].length do
     if team == game_hash[:home][:team_name]
-      brooklyn_numbers.push(game_hash)[:home][:players][count][:number]
+      brooklyn_numbers.push.game_hash[:home][:players][count][:number]
     end
 
     count += 1
   end
   while count < game_hash[:away][:players].length do
     if team == game_hash[:away][:team_name]
-      charlotte_numbers.push(game_hash)[:away][:players][count][:number]
+      charlotte_numbers.push.game_hash[:away][:players][count][:number]
     end
 
     count += 1
