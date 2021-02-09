a456b88fb80247b229dc4e1c9b4b3f8304a6cfa8
Radiyah Abdus-Sabur <rsabur58@gmail.com>
2021-02-09 16:56:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -174,7 +174,7 @@ def player_numbers(team)
   count = 0
   while count < game_hash[:home][:players].length
     if team == game_hash[:home][:team_name]
-      return game_hash[:home][:players][count][:number].join
+      return game_hash[:home][:players][count][:number].join(*)
     end
     count += 1
   end
